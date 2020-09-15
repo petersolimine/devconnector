76adeeae559e2c177c17eaaca7a6a9e9755b92d4
petersolimine <psolimine@tulane.edu>
2020-09-14 20:43:50 -0500

    new commit

--- server.js
+++ server.js
@@ -15,19 +15,19 @@ app.get('/', (req, res) => res.send('API Running'));
 //define routes
 app.use(
   '/api/users',
-  require('C:\\Users\\peter\\OneDrive\\Documents\\AA - Fake Sem\\devconnector\\config\\routes\\api\\users')
+  require('..\\devconnector\\config\\routes\\api\\users')
 );
 app.use(
   '/api/auth',
-  require('C:\\Users\\peter\\OneDrive\\Documents\\AA - Fake Sem\\devconnector\\config\\routes\\api\\auth')
+  require('..\\devconnector\\config\\routes\\api\\auth')
 );
 app.use(
   '/api/profile',
-  require('C:\\Users\\peter\\OneDrive\\Documents\\AA - Fake Sem\\devconnector\\config\\routes\\api\\profile')
+  require('..\\devconnector\\config\\routes\\api\\profile')
 );
 app.use(
   '/api/posts',
-  require('C:\\Users\\peter\\OneDrive\\Documents\\AA - Fake Sem\\devconnector\\config\\routes\\api\\posts')
+  require('..\\devconnector\\config\\routes\\api\\posts')
 );
 
 const PORT = process.env.PORT || 5000;
