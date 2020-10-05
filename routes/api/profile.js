671fb0f2b2ae81d7659dfe91438d517fe7145811
Peter Solimine <petersolimine@Peters-MacBook-Pro.local>
2020-10-04 23:18:11 -0500

    posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const router = express.Router();
 const auth = require('../../../middleware/auth');
 const {check, validationResult } = require('express-validator/check');
@@ -262,6 +263,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     } catch (err) {
         console.log(err.message);
         res.status(500).send('Server Error');
+    }
+})
+// @route   GET api/profile/github/:username
+// @desc    get user repos from github
+// @access  Public
+
+router.get('/github/:username',  async (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&cient_id=${config.get('githubClientID')}&cient_secret=${config.get('githubSecret')}`,
+            method:GET
+            headers: {'user-agent': 'node.js'}
+        };
+
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+
+            if(response.statusCode !== 200){
+                return res.status(404).json({ msg: 'no github profile found' });
+            }
+
+            res.json(JSON.parse(body));
+        })
+    } catch (err) {
+        consoe.error(err.message);
+        res.status(500.send('Server Error');)
+        
     }
 })
 
