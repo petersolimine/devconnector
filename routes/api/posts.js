a07cf432054e944836e940f8f26c1308c0bce484
Peter Solimine <petersolimine@Peters-MacBook-Pro.local>
2020-10-07 16:37:17 -0500

    delete likes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -84,7 +84,6 @@ router.delete('/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(rew.params.id);
 
-
         if(!post) {
             return res.status(404).json({msg: 'Post not found'});
         }
@@ -107,5 +106,54 @@ router.delete('/:id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+
+router.put('/like/:id', auth, async (req, res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //check if post has already been liked
+        if(post.likes.filter(like => like.user.toString() === request.user.id).length > 0){
+            return res.status(400).json({msg: 'Post already liked'});
+        }
+        post.likes.unshift({user: req.user.id});
+
+        await post.save();
+
+        res.json(post,likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+// @route   PUT api/posts/unlike/:id
+// @desc    unlike a post
+// @access  Private
+
+router.put('/like/:id', auth, async (req, res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //check if post has already been liked
+        if(post.likes.filter(like => like.user.toString() === request.user.id).length === 0){
+            return res.status(400).json({msg: 'Post has not yet been liked'});
+        }
+        //get the remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post,likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
